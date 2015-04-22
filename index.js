ee4c2cdba16a869352d72c48a79394737b6b23a3
Tim Macfarlane <timmacfarlane@gmail.com>
2015-04-22 12:23:44 +0200

    medium-editor for plastiq

---
+++ index.js
@@ -0,0 +1,47 @@
+var plastiq = require('plastiq');
+var h = require('plastiq').html;
+var Medium = require('medium-editor');
+
+module.exports = function (options) {
+  var binding = plastiq.binding(options.binding);
+
+  return h.component(
+    {
+      onadd: function (element) {
+        var self = this;
+
+        this.html = binding.get();
+        element.innerHTML = normaliseHtml(this.html);
+
+        this.editor = new Medium(element, {
+          buttons: ['bold', 'italic', 'header1', 'header2', 'unorderedlist', 'orderedlist']
+        });
+
+        element.addEventListener('input', function (ev) {
+          self.html = ev.target.innerHTML;
+          binding.set(self.html);
+        });
+      },
+      onupdate: function (element) {
+        var html = binding.get();
+
+        if (this.html != html) {
+          this.html = html;
+          element.innerHTML = normaliseHtml(html);
+        }
+      },
+      onremove: function () {
+        this.editor.destroy();
+      }
+    },
+    h('div', {class: options.class})
+  );
+};
+
+function normaliseHtml(html) {
+  if (html) {
+    return html;
+  } else {
+    return '<p><br></p>';
+  }
+}
